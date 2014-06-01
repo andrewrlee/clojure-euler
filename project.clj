651ed1955024e4ad60d52f83126d768f482188be
Andy <plasma147@gmail.com>
2014-06-01 17:06:55 +0100

    fixing midje

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject euler "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.trace "0.7.8"]]
-  :main ^:skip-aot euler.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {
+             :dev {
+                   :dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
+  
